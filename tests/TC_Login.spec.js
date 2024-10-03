cd4814e49aeee150678383b91bc46aa99e9ba33c
Venkatakrishnan Jayakumar <venkatakrishnan@virtusa.com>
2024-10-03 13:06:10 +0530

    Landingpagescreen_FirstCommit

---
+++ tests/TC_Login.spec.js
@@ -0,0 +1,49 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://lighthouse-dev.onrender.com/#our-impact');
+  await expect(page).toHaveTitle('Lighthouse');
+  //await page.waitForTimeout(3000);
+  await page.evaluate(()=>{ 
+    window.scrollBy(0,1000);
+    
+  })
+//our-impact
+await page.locator("xpath=//*[@title='Our Impact']").click();
+
+//our-services
+await page.locator("xpath=//*[@title='Our Services']").click();
+await page.waitForTimeout(1000);
+await page.evaluate(()=>{ 
+  window.scrollBy(0,300);
+  
+});
+//how does it work 
+await page.locator("xpath=//*[@title='How does it work']").click();
+await page.waitForTimeout(1000);
+//FAQS
+await page.locator("xpath=//*[@title='FAQs']").click();
+await page.waitForTimeout(1000);
+await page.evaluate(()=>{ 
+  window.scrollBy(0,300);
+  
+});
+await page.locator("xpath=(//*[@loading='lazy'])[14]").click();
+await page.waitForTimeout(1000);
+
+
+await page.locator("xpath=(//*[@loading='lazy'])[15]").click();
+await page.waitForTimeout(1000);
+await page.evaluate(() => {
+window.scrollTo(0, 1000); // Scrolls 500 pixels down
+});
+await page.locator("xpath=(//*[@loading='lazy'])[16]").click();
+await page.waitForTimeout(1000);
+await page.evaluate(() => {
+ window.scrollTo(0, 1000); // Scrolls 500 pixels down
+ });
+await page.locator("xpath=(//*[@loading='lazy'])[17]").click();
+await page.waitForTimeout(1000);
+
+});
